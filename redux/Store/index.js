d521aee55ee020d08fb37efdf932ae4fb8f765f8
Prabhjeev <prabhjeev@Prabhjeevs-MacBook-Air-2.local>
2023-01-19 08:30:50 +0530

    live api

--- redux/Store/index.js
+++ redux/Store/index.js
@@ -9,7 +9,7 @@ let store;
 
 function initStore(initialState) {
   const axiosInstance = axios.create({
-    baseURL: baseUrl || 'http://localhost:3001',
+    baseURL: baseUrl || 'https://dsa-help-platform.onrender.com',
   });
   return configureStore({
     reducer: rootReducers,
